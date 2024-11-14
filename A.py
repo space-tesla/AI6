533779e03a420fa5cca7d08aaacb4b61a967ae55
space-tesla <aspyrontech@gmail.com>
2024-11-14 19:59:16 +0530

    comments removed sucessfully !!!

--- A.py
+++ A.py
@@ -1,13 +1,11 @@
 #Q.1) Write a Python program to print lowercase and uppercase alphabets of a string
 
-# Define a string
+
 text = "HelloWorld"
 
-# Print lowercase alphabets
 lowercase = [char for char in text if char.islower()]
 print("Lowercase letters:", lowercase)
 
-# Print uppercase alphabets
 uppercase = [char for char in text if char.isupper()]
 print("Uppercase letters:", uppercase)
 
